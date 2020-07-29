36a11d0b1f773c75ab87962d842483a9f6496c05
venkg <writeto_venkatesh@yahoo.com>
2020-07-29 14:24:07 +0530

    Update AuditInfo.java
    OS Update fails in scenario where we are pass empty child object and second time with value same as parent. Including "from" attribute resolves the issue. Please check and include the changes.

--- java/pojos/src/main/java/io/opensaber/pojos/AuditInfo.java
+++ java/pojos/src/main/java/io/opensaber/pojos/AuditInfo.java
@@ -1,11 +1,14 @@
 package io.opensaber.pojos;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+@JsonIgnoreProperties
 public class AuditInfo {
     private String op;
     private String path;
     private Object value;
+    private String from;
 
     public String getOp() {
         return op;
@@ -22,6 +25,10 @@ public class AuditInfo {
     public void setPath(String path) {
         this.path = path;
     }
+
+    public String getFrom() { return from; }
+
+    public void setFrom(String from) { this.from = from; }
 
     @JsonIgnore
     public Object getValue() {
